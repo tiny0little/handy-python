551f4955640f1befaac43698de26a3208651cfbb
Nickolay Romensky <nromensky@gmail.com>
2021-05-16 12:36:41 +0300

    .

--- active_plots.py
+++ active_plots.py
@@ -8,11 +8,10 @@ from datetime import *
 from tabulate import tabulate
 import os
 
-
 log_location = "/home/user/.chia/mainnet/plotter"
-log_files = glob.glob(f"{log_location}/*.log")
 start_line = 1
-final_table = []
+final_completed_table = []
+final_active_table = []
 plot_size = []
 phase1 = []
 phase1time = []
@@ -31,24 +30,44 @@ def phase_time(_phase_number):
     if len(_output) > 1:
         _output = int(float(_output[0].split("=")[1].strip()))
         _min, _sec = divmod(_output, 60)
-        _hours, _mim = divmod(_min, 60)
-        return f"{_hours:02d}:{_mim:02d}"
+        _hours, _min = divmod(_min, 60)
+        return f"{_hours:02d}:{_min:02d}"
     else:
         return ""
 
 
 #
 #
+final_completed_table.append(["log", "k", "completed in"])
+log_files = glob.glob(f"{log_location}/*.log")
 for log_file in log_files:
+
+    # checking if any completed plots in the logfile
+    line1 = 1
+    line2 = 1
+    output = subprocess.getoutput(f"grep -n 'Total time' {log_file}").split("\n")
+    for line in output:
+        line2 = line.split(":")[0].strip()
+        if line2.isnumeric():
+            plot_size0 = subprocess.getoutput(f"awk 'NR <= {line2} && NR >= {line1}' {log_file} "
+                                              "| egrep 'Plot size is'").split(":")[1].strip()
+            total_time = int(float(subprocess.getoutput(f"awk 'NR <= {line2} && NR >= {line1}' {log_file} | egrep "
+                                                        "'Total time' ").split("seconds.")[0].strip().split("=")[
+                                       1].strip()))
+            min0, sec0 = divmod(total_time, 60)
+            hours0, min0 = divmod(min0, 60)
+            final_completed_table.append([os.path.basename(log_file), plot_size0, f"{hours0:02d}:{min0:02d}"])
+
+    #
     # starting line of current plot
     start_line = int(subprocess.getoutput(f"egrep -n 'Starting plotting progress into temporary' {log_file} "
-                                      "| tail -1").split(":")[0].strip())
+                                          "| tail -1").split(":")[0].strip())
 
     start_time = parse(subprocess.getoutput(f"awk 'NR >= {start_line}' {log_file} | egrep "
                                             "'Starting phase 1/4: Forward Propagation' "
                                             "| tail -1").split("...")[1].strip())
     running_time = relativedelta(datetime.now(), start_time)
-    output = subprocess.getoutput(f"awk 'NR >= {start_line}' {log_file} | egrep 'Plot size is' | tail -1")
+    output = subprocess.getoutput(f"awk 'NR >= {start_line}' {log_file} | egrep 'Plot size is'")
     plot_size.append(output.split(":")[1].strip())
 
     #
@@ -58,7 +77,8 @@ for log_file in log_files:
         output1 = ""
         for i in range(int(output)): output1 += f"{i + 1},"
         phase1.append(output1[:-1])
-    else: phase1.append("")
+    else:
+        phase1.append("")
 
     #
     #
@@ -72,7 +92,8 @@ for log_file in log_files:
         for i in range(int(output)): output1 += f"{7 - i},"
         if output1[-2:] == "2,": output1 += "1,"
         phase2.append(output1[:-1])
-    else: phase2.append("")
+    else:
+        phase2.append("")
 
     #
     #
@@ -85,7 +106,8 @@ for log_file in log_files:
         output1 = ""
         for i in range(int(output)): output1 += f"{1 + i},"
         phase3.append(output1[:-1])
-    else: phase3.append("")
+    else:
+        phase3.append("")
 
     #
     #
@@ -94,8 +116,10 @@ for log_file in log_files:
     #
     #
     output = subprocess.getoutput(f"awk 'NR >= {start_line}' {log_file} | egrep 'Starting phase 4/4' | wc -l")
-    if int(output) > 0: phase4.append(".")
-    else: phase4.append("")
+    if int(output) > 0:
+        phase4.append(".")
+    else:
+        phase4.append("")
 
     #
     #
@@ -105,13 +129,14 @@ for log_file in log_files:
     #
     running_time_tab.append(f"{running_time.days * 24 + running_time.hours:02d}:{running_time.minutes:02d}")
 
-
-final_table.append(["log", "k", "p1", "p1 time", "p2", "p2 time", "p3",
-                    "p3 time", "p4", "p4 time", "runtime"])
+final_active_table.append(["log", "k", "p1", "p1 time", "p2", "p2 time", "p3", "p3 time", "p4", "p4 time", "runtime"])
 for i in range(len(log_files)):
-    final_table.append([os.path.basename(log_files[i]), plot_size[i], phase1[i], phase1time[i],
-                        phase2[i], phase2time[i], phase3[i], phase3time[i], phase4[i], phase4time[i],
-                        running_time_tab[i]])
+    final_active_table.append([os.path.basename(log_files[i]), plot_size[i], phase1[i], phase1time[i],
+                               phase2[i], phase2time[i], phase3[i], phase3time[i], phase4[i], phase4time[i],
+                               running_time_tab[i]])
 
-tab_align = ['left','left','right','left','right','left','right','left','right','left','left']
-print(tabulate(final_table, colalign=tab_align, headers="firstrow", tablefmt="orgtbl"))
+tab_align = ['left', 'left', 'right']
+print(tabulate(final_completed_table, colalign=tab_align, headers="firstrow", tablefmt="orgtbl"))
+print()
+tab_align = ['left', 'left', 'right', 'left', 'right', 'left', 'right', 'left', 'right', 'left', 'left']
+print(tabulate(final_active_table, colalign=tab_align, headers="firstrow", tablefmt="orgtbl"))
--- count_plots.py
+++ count_plots.py
@@ -3,33 +3,32 @@
 import subprocess
 import argparse
 import os
+from halo import Halo
 
 parser = argparse.ArgumentParser(description="counting CHIA plots")
 parser.add_argument("-k", type=int, help="plot size (32, 33, ...)")
 args = parser.parse_args()
 
-
 if args.k is None:
-  plots = subprocess.getoutput(f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null").split("\n")
+    with Halo(color='white'):
+        plots = subprocess.getoutput(f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null").split("\n")
 else:
-  plots = subprocess.getoutput(f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null | egrep 'k{args.k}-'").split("\n")
+    with Halo(color='white'):
+        plots = subprocess.getoutput(
+            f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null | egrep 'k{args.k}-'").split("\n")
 
 if plots[0] == '':
-  plots.pop(0)
-
+    plots.pop(0)
 
 size = 0
 for plot in plots:
-  size += os.path.getsize(plot)
+    size += os.path.getsize(plot)
 
-size = size / (1024*1024*1024*1024)
-
+size = size / (1024 * 1024 * 1024 * 1024)
 
 if args.k is None:
-  print(f"total plots count : {len(plots)}")
-  print(f"total plots size  : {size:.2f} TB")
+    print(f"total plots count : {len(plots)}")
+    print(f"total plots size  : {size:.2f} TB")
 else:
-  print(f"plot k{args.k} count : {len(plots)}")
-  print(f"plot k{args.k} size  : {size:.2f} TB")
-
-
+    print(f"plot k{args.k} count : {len(plots)}")
+    print(f"plot k{args.k} size  : {size:.2f} TB")
