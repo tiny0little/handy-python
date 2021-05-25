ca8ef77b44cf39c8675f3475b48d2bd92d79a2d4
Nickolay Romensky <nromensky@gmail.com>
2021-05-25 16:09:42 +0300

    .

--- count_plots.py
+++ count_plots.py
@@ -4,6 +4,9 @@ import subprocess
 import argparse
 import os
 from halo import Halo
+from tabulate import tabulate
+
+finalTable = []
 
 parser = argparse.ArgumentParser(description="counting CHIA plots")
 parser.add_argument("-k", type=int, help="plot size (32, 33, ...)")
@@ -11,36 +14,46 @@ args = parser.parse_args()
 
 with Halo(color='white'):
     netspace0 = subprocess.getoutput("cd ~/src/chia-blockchain/ && . ./activate && chia netspace "
-                                      " | egrep 'The network has an estimated'")
-    netspace0 = netspace0.split("he network has an estimated")[1].split("EiB")[0].strip()
-    # in EiB
-    netspace = float(netspace0)
+                                     " | egrep 'The network has an estimated'")
+    netspace0 = netspace0.split("The network has an estimated")[1].split("EiB")[0].strip()
+    netspace = float(netspace0)  # in EiB
 
-if args.k is None:
-    with Halo(color='white'):
-        plots = subprocess.getoutput(f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null").split("\n")
-else:
-    with Halo(color='white'):
+    #
+    time_to_win = subprocess.getoutput("cd ~/src/chia-blockchain/ && . ./activate && chia farm summary "
+                                       " | egrep 'Expected time to win'")
+    time_to_win = time_to_win.split("Expected time to win:")[1].strip()
+
+    #
+    if args.k is None:
+        plots = subprocess.getoutput(
+            f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null").split("\n")
+    else:
         plots = subprocess.getoutput(
             f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null | egrep 'k{args.k}-'").split("\n")
 
-if plots[0] == '':
-    plots.pop(0)
+    if plots[0] == '':
+        plots.pop(0)
 
-size = 0
-for plot in plots:
-    size += os.path.getsize(plot)
+    size = 0
+    for plot in plots:
+        size += os.path.getsize(plot)
 
+#
 # convert bytes to TiB
 size = size / 1.099511628e+12
 
-
 if args.k is None:
-    print(f"total plots count     : {len(plots)}")
-    print(f"total plots size      : {size:.3f} TiB")
-    print(f"total XCH netspace    : {netspace:.3f} EiB")
-    print(f"my space contribution : {(size / (netspace * 1.049e+6)):.7f}%")
-    print(f"possible reward       : {(4608 * 2 * size / (netspace * 1.049e+6)):.4f} XCH per day")
+    finalTable.append(["total plots count", len(plots)])
+    finalTable.append(["total plots  size", f"{size:.3f} TiB"])
+    finalTable.append(["total XCH netspace", f"{netspace:.3f} EiB"])
+    finalTable.append(["my space contribution", f"{(size / (netspace * 1.049e+6)):.7f}%"])
+    finalTable.append(["possible reward", f"{(4608 * 2 * size / (netspace * 1.049e+6)):.4f} XCH per day"])
+    finalTable.append(["expected time to win", time_to_win])
 else:
-    print(f"plot k{args.k} count : {len(plots)}")
-    print(f"plot k{args.k} size  : {size:.2f} TB")
+    # print(f"plot k{args.k} count : {len(plots)}")
+    finalTable.append([f"plot-k{args.k} count", f"{len(plots)}"])
+    # print(f"plot k{args.k} size  : {size:.2f} TB")
+    finalTable.append([f"plot-k{args.k}  size", f"{size:.2f} TB"])
+
+row0 = ['right', 'left']
+print(tabulate(finalTable, colalign=row0, tablefmt="pretty"))
--- plot_logs.py
+++ plot_logs.py
@@ -9,6 +9,7 @@ from tabulate import tabulate
 import os
 import argparse
 from operator import itemgetter
+from halo import Halo
 
 log_location = "/home/user/.chia/mainnet/plotter"
 final_completed_table = []
@@ -98,7 +99,7 @@ def get_final_dir(_log_file: str, _start_line: int, _end_line: int) -> str:
 #
 parser = argparse.ArgumentParser(description="plot logs analyzer")
 parser.add_argument("-d", "--dir", help=f"location of plot logs. default {log_location}")
-parser.add_argument("-s", "--sort", choices=['plot', 'time'], nargs='?', default='plot', const='plot',
+parser.add_argument("-s", "--sort", choices=['plot', 'p', 'time', 't'], nargs='?', default='plot', const='plot',
                     help="which column used for sorting the table? (default: %(default)s)")
 parser.add_argument("-nc", "--nocolor", help="do not use colors", action="store_true")
 args = parser.parse_args()
@@ -126,83 +127,84 @@ final_running_table.append(
      "p1 time", "p2", "p2 time", "p3", "p3 time", "p4", "p4 time", "runtime"])
 log_files = glob.glob(f"{log_location}/*log")
 
-for log_file in log_files:
+with Halo(color='white'):
+    for log_file in log_files:
 
-    start_line = 1
-    end_line = 1
-
-    #
-    # processing completed plots
-    #
-    output = subprocess.getoutput(f"grep -n 'Copy time' {log_file}").split("\n")
-    for line in output:
-        end_line0 = line.split(":")[0].strip()
-        if end_line0.isnumeric():
-            end_line = int(end_line0)
-            final_completed_table.append(
-                [os.path.basename(log_file), get_temp_dir(log_file, start_line, end_line),
-                 get_final_dir(log_file, start_line, end_line),
-                 get_plot_size(log_file, start_line, end_line),
-                 get_buckets(log_file, start_line, end_line),
-                 get_buffer_size(log_file, start_line, end_line),
-                 get_time('Time for phase 1', log_file, start_line, end_line),
-                 get_time('Time for phase 2', log_file, start_line, end_line),
-                 get_time('Time for phase 3', log_file, start_line, end_line),
-                 get_time('Time for phase 4', log_file, start_line, end_line),
-                 get_time("Copy time", log_file, start_line, end_line),
-                 get_time2("Total time", "Copy time", log_file, start_line, end_line)])
-
-        start_line = end_line + 1
-
-    #
-    # processing currently running plot
-    #
-    # start_line
-    str0 = f"egrep -n 'Starting plotting progress into temporary' {log_file} | tail -1"
-    if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
-        start_line = int(subprocess.getoutput(f"{str0}").split(":")[0].strip())
-    else:
         start_line = 1
+        end_line = 1
 
-    #
-    # end_line
-    end_line = int(subprocess.getoutput(f"cat {log_file} | wc -l").strip())
+        #
+        # processing completed plots
+        #
+        output = subprocess.getoutput(f"grep -n 'Copy time' {log_file}").split("\n")
+        for line in output:
+            end_line0 = line.split(":")[0].strip()
+            if end_line0.isnumeric():
+                end_line = int(end_line0)
+                final_completed_table.append(
+                    [os.path.basename(log_file), get_temp_dir(log_file, start_line, end_line),
+                     get_final_dir(log_file, start_line, end_line),
+                     get_plot_size(log_file, start_line, end_line),
+                     get_buckets(log_file, start_line, end_line),
+                     get_buffer_size(log_file, start_line, end_line),
+                     get_time('Time for phase 1', log_file, start_line, end_line),
+                     get_time('Time for phase 2', log_file, start_line, end_line),
+                     get_time('Time for phase 3', log_file, start_line, end_line),
+                     get_time('Time for phase 4', log_file, start_line, end_line),
+                     get_time("Copy time", log_file, start_line, end_line),
+                     get_time2("Total time", "Copy time", log_file, start_line, end_line)])
 
-    #
-    # start_tine of current plot
-    str0 = f"awk 'NR >= {start_line}' {log_file} | egrep 'Starting phase 1/4: Forward Propagation' | tail -1"
-    if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
-        start_time = parse(subprocess.getoutput(f"{str0}").split("...")[1].strip())
-    else:
-        start_time = datetime.now()
+            start_line = end_line + 1
 
-    running_time = relativedelta(datetime.now(), start_time)
+        #
+        # processing currently running plot
+        #
+        # start_line
+        str0 = f"egrep -n 'Starting plotting progress into temporary' {log_file} | tail -1"
+        if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
+            start_line = int(subprocess.getoutput(f"{str0}").split(":")[0].strip())
+        else:
+            start_line = 1
 
-    final_running_table.append([os.path.basename(log_file), get_temp_dir(log_file, start_line, end_line),
-                                get_plot_size(log_file, start_line, end_line),
-                                get_buckets(log_file, start_line, end_line),
-                                get_buffer_size(log_file, start_line, end_line),
-                                get_phase_progress("Computing table", 7, log_file, start_line, end_line),
-                                get_time('Time for phase 1', log_file, start_line, end_line),
-                                get_phase_progress("Backpropagating on table", 6, log_file, start_line, end_line),
-                                get_time('Time for phase 2', log_file, start_line, end_line),
-                                get_phase_progress("Compressing tables", 6, log_file, start_line, end_line),
-                                get_time('Time for phase 3', log_file, start_line, end_line),
-                                get_phase_progress("Starting phase 4/4", 1, log_file, start_line, end_line),
-                                get_time('Time for phase 4', log_file, start_line, end_line),
-                                f"{running_time.days * 24 + running_time.hours:02d}h{running_time.minutes:02d}m"
-                                ])
+        #
+        # end_line
+        end_line = int(subprocess.getoutput(f"cat {log_file} | wc -l").strip())
+
+        #
+        # start_tine of current plot
+        str0 = f"awk 'NR >= {start_line}' {log_file} | egrep 'Starting phase 1/4: Forward Propagation' | tail -1"
+        if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
+            start_time = parse(subprocess.getoutput(f"{str0}").split("...")[1].strip())
+        else:
+            start_time = datetime.now()
+
+        running_time = relativedelta(datetime.now(), start_time)
+
+        final_running_table.append([os.path.basename(log_file), get_temp_dir(log_file, start_line, end_line),
+                                    get_plot_size(log_file, start_line, end_line),
+                                    get_buckets(log_file, start_line, end_line),
+                                    get_buffer_size(log_file, start_line, end_line),
+                                    get_phase_progress("Computing table", 7, log_file, start_line, end_line),
+                                    get_time('Time for phase 1', log_file, start_line, end_line),
+                                    get_phase_progress("Backpropagating on table", 6, log_file, start_line, end_line),
+                                    get_time('Time for phase 2', log_file, start_line, end_line),
+                                    get_phase_progress("Compressing tables", 6, log_file, start_line, end_line),
+                                    get_time('Time for phase 3', log_file, start_line, end_line),
+                                    get_phase_progress("Starting phase 4/4", 1, log_file, start_line, end_line),
+                                    get_time('Time for phase 4', log_file, start_line, end_line),
+                                    f"{running_time.days * 24 + running_time.hours:02d}h{running_time.minutes:02d}m"
+                                    ])
 
 if len(final_completed_table) > 1:
-    if args.sort == 'plot': final_completed_table[1:] = sorted(final_completed_table[1:], key=itemgetter(1))
-    if args.sort == 'time': final_completed_table[1:] = sorted(final_completed_table[1:], key=itemgetter(-1))
+    if args.sort[0] == 'p': final_completed_table[1:] = sorted(final_completed_table[1:], key=itemgetter(1))
+    if args.sort[0] == 't': final_completed_table[1:] = sorted(final_completed_table[1:], key=itemgetter(-1))
     tab_align = ['left', 'left', 'left', 'left', 'left', 'left', 'center', 'center', 'center', 'center',
                  'center', 'center']
     print(tabulate(final_completed_table, colalign=tab_align, headers="firstrow", tablefmt="pretty"))
 
 if len(final_running_table) > 1:
-    if args.sort == 'plot': final_running_table[1:] = sorted(final_running_table[1:], key=itemgetter(1))
-    if args.sort == 'time': final_running_table[1:] = sorted(final_running_table[1:], key=itemgetter(-1))
+    if args.sort[0] == 'p': final_running_table[1:] = sorted(final_running_table[1:], key=itemgetter(1))
+    if args.sort[0] == 't': final_running_table[1:] = sorted(final_running_table[1:], key=itemgetter(-1))
     tab_align = ['left', 'left', 'left', 'left', 'left', 'center', 'center', 'center', 'center', 'center',
                  'center', 'center', 'center', 'center']
     print(tabulate(final_running_table, colalign=tab_align, headers="firstrow", tablefmt="pretty"))
