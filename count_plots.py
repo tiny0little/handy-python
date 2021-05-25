27b53d5e67691d813e57822b1e4549f03de0d772
Nickolay Romensky <nromensky@gmail.com>
2021-05-25 14:31:51 +0300

    .

--- count_plots.py
+++ count_plots.py
@@ -9,6 +9,13 @@ parser = argparse.ArgumentParser(description="counting CHIA plots")
 parser.add_argument("-k", type=int, help="plot size (32, 33, ...)")
 args = parser.parse_args()
 
+with Halo(color='white'):
+    netspace0 = subprocess.getoutput("cd ~/src/chia-blockchain/ && . ./activate && chia netspace "
+                                      " | egrep 'The network has an estimated'")
+    netspace0 = netspace0.split("he network has an estimated")[1].split("EiB")[0].strip()
+    # in EiB
+    netspace = float(netspace0)
+
 if args.k is None:
     with Halo(color='white'):
         plots = subprocess.getoutput(f"find /home/ /media/ -path '*CHIA*' -name 'plot*plot' 2> /dev/null").split("\n")
@@ -24,11 +31,16 @@ size = 0
 for plot in plots:
     size += os.path.getsize(plot)
 
-size = size / (1024 * 1024 * 1024 * 1024)
+# convert bytes to TiB
+size = size / 1.099511628e+12
+
 
 if args.k is None:
-    print(f"total plots count : {len(plots)}")
-    print(f"total plots size  : {size:.2f} TB")
+    print(f"total plots count     : {len(plots)}")
+    print(f"total plots size      : {size:.3f} TiB")
+    print(f"total XCH netspace    : {netspace:.3f} EiB")
+    print(f"my space contribution : {(size / (netspace * 1.049e+6)):.7f}%")
+    print(f"possible reward       : {(4608 * 2 * size / (netspace * 1.049e+6)):.4f} XCH per day")
 else:
     print(f"plot k{args.k} count : {len(plots)}")
     print(f"plot k{args.k} size  : {size:.2f} TB")
