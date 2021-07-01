fcb00b39b94ca754725e039e32b13f5c91bf0c1a
user <nromensky@gmail.com>
2021-07-01 15:07:07 +0300

    .

---
+++ leetcode/0065.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.8
+
+"""
+65. Valid Number
+https://leetcode.com/problems/valid-number/
+
+"""
+
+
+class Solution:
+    def isNumber(self, s: str) -> bool: \
+            pass
+
+
+#
+#
+
+sol = Solution()
+print(sol.isNumber(s=".1"))
