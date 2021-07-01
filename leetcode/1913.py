bb701438207a93579717241045543fb808756250
Nickolay Romensky <nromensky@gmail.com>
2021-07-01 18:27:00 +0300

    .

---
+++ leetcode/1913.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.8
+
+"""
+1913. Maximum Product Difference Between Two Pairs
+https://leetcode.com/contest/weekly-contest-247/problems/maximum-product-difference-between-two-pairs/
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+
+
+#
+#
+
+sol = Solution()
+print(sol.maxProductDifference(nums=[5, 6, 2, 7, 4]))
