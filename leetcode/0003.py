572530ba2a5f3337569aaf542d1685c3ad48d431
Nickolay Romensky <nromensky@gmail.com>
2021-07-05 05:01:47 +0300

    .

--- leetcode/0003.py
+++ leetcode/0003.py
@@ -4,15 +4,16 @@
 3. Longest Substring Without Repeating Characters
 Medium
 
+https://www.geeksforgeeks.org/print-longest-substring-without-repeating-characters/
 """
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        right = len(s)
-        while True:
-            sub_str = s[left:right]
+        substr_maxlen = 0
+        substr_start_index = 0
+
+        for i in range(len(s)):
 
 
 sol = Solution()
