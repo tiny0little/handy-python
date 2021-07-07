2ef3f11ff8763682cb69c7c90963cec1e7b650a8
Nickolay Romensky <nromensky@gmail.com>
2021-07-07 12:15:49 +0300

    .

--- leetcode/1904.py
+++ leetcode/1904.py
@@ -2,36 +2,43 @@
 """
 1904. The Number of Full Rounds You Have Played
 Difficulty: Medium
-
+Success
+Runtime: 28 ms, faster than 86.71% of Python3 online submissions
+Memory Usage: 14.4 MB, less than 29.32% of Python3 online submissions
 """
 
 
 class Solution:
+
     def numberOfRounds(self, startTime: str, finishTime: str) -> int:
-        result = 0
+        # counting hours
         start_hour = int(startTime[:2])
         finish_hour = int(finishTime[:2])
-
-        if finish_hour > start_hour:
-            result = (finish_hour - start_hour) * 4
-        elif start_hour > finish_hour:
-            result = (24 - start_hour + finish_hour) * 4
-
         start_min = int(startTime[-2:])
-        
         finish_min = int(finishTime[-2:])
+        result = 24 + finish_hour - start_hour
+        if result > 24:
+            result -= 24
+        elif (result == 24) and (finish_min > start_min):
+            result -= 24
 
-        if finish_hour == start_hour:
-            result += finish_min - start_min
-        else:
-            result += finish_min + start_min
+        # counting minutes
+        if start_min % 15 != 0: start_min += 15 - start_min % 15
+        finish_min -= finish_min % 15
 
-        return result
+        result *= 60
+        result += finish_min - start_min
+
+        if result < 0: result = 0
+        return int(result / 15)
 
 
 sol = Solution()
-print(sol.numberOfRounds(startTime="12:01", finishTime="12:47"))
+print(sol.numberOfRounds(startTime="00:01", finishTime="00:02"))
 
-# if sol.numberOfRounds(startTime="12:01", finishTime="12:44") != 1: print('err-1')
-# if sol.numberOfRounds(startTime="20:00", finishTime="06:00") != 40: print('err-2')
-# if sol.numberOfRounds(startTime="00:00", finishTime="23:59") != 95: print('err-3')
+if sol.numberOfRounds(startTime="12:01", finishTime="12:44") != 1: print('err-1')
+if sol.numberOfRounds(startTime="20:00", finishTime="06:00") != 40: print('err-2')
+if sol.numberOfRounds(startTime="00:00", finishTime="23:59") != 95: print('err-3')
+if sol.numberOfRounds(startTime="00:01", finishTime="00:00") != 95: print('err-4')
+if sol.numberOfRounds(startTime="00:01", finishTime="00:02") != 0: print('err-5')
+if sol.numberOfRounds(startTime="00:00", finishTime="00:14") != 0: print('err-6')
