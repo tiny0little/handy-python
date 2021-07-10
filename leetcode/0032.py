69c4a7f02fac1d4eddd684da0d16841d292267ac
Nickolay Romensky <nromensky@gmail.com>
2021-07-10 05:21:32 +0300

    .

---
+++ leetcode/0032.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+32. Longest Valid Parentheses
+Difficulty: Hard
+
+"""
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if len(s) < 2: return 0
+
+
+sol = Solution()
+print(sol.longestValidParentheses(s="(()"))
+
+if sol.longestValidParentheses(s="(()") != 2: print('err-1')
+if sol.longestValidParentheses(s=")()())") != 4: print('err-2')
+if sol.longestValidParentheses(s="") != 0: print('err-3')
