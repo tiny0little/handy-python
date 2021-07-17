ab8689c0e5e964db57da48cc273def33b86a5afc
Nickolay Romensky <nromensky@gmail.com>
2021-07-17 05:49:02 +0300

    .

--- leetcode/0216.py
+++ leetcode/0216.py
@@ -35,5 +35,5 @@ class Solution:
 
 sol = Solution()
 stime = time.time()
-print(sol.combinationSum3(k=9, n=25))
+print(sol.combinationSum3(k=9, n=45))
 print(f'runtime: {time.time() - stime:.2f}sec')
