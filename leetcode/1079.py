0c7bea278ec92345c73602537c70886f33b1dd23
Nickolay Romensky <nromensky@gmail.com>
2021-07-17 15:54:39 +0300

    .

---
+++ leetcode/1079.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+1079. Letter Tile Possibilities
+
+"""
+from typing import List
+import time
+
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+
+
+sol = Solution()
