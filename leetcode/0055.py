e98f2e23876b805092d8ce41d0884526e9b2ac9d
Nickolay Romensky <nromensky@gmail.com>
2021-07-18 12:11:53 +0300

    .

---
+++ leetcode/0055.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+"""
+from typing import List
+import time
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+
+sol = Solution()
+stime = time.time()
+
+print(f'runtime: {time.time() - stime:.2f}sec')
