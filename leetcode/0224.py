6a1a74e06219bb399b67de3e353a1de804f20468
user <nromensky@gmail.com>
2021-07-23 16:43:27 +0300

    .

---
+++ leetcode/0224.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+224. Basic Calculator
+Difficulty: Hard
+"""
+from typing import List
+import time
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+
+
+sol = Solution()
+stime = time.time()
+print(sol.combinationSum3(k=9, n=45))
+print(f'runtime: {time.time() - stime:.2f}sec')
