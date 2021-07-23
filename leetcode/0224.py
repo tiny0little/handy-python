202bc91a3c54a25a04de964794f5771cd646828b
user <nromensky@gmail.com>
2021-07-23 17:21:56 +0300

    .

--- leetcode/0224.py
+++ leetcode/0224.py
@@ -10,8 +10,15 @@ import time
 class Solution:
     def calculate(self, s: str) -> int:
 
+        def calc_unit(s0: str) -> int:
+            sub_unit = [-1, -1]
+            i = 0
+            while True:
+                if s0[i] == '(':
+                    sub_unit[0] = i + 1
+                    
 
 sol = Solution()
 stime = time.time()
-print(sol.combinationSum3(k=9, n=45))
+print(sol.calculate(s="(1+(4+5+2)-3)+(6+8)"))
 print(f'runtime: {time.time() - stime:.2f}sec')
