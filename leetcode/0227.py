3b013a37e4daf0ecd2ac02dc7941737502d7513b
user <nromensky@gmail.com>
2021-07-25 17:25:00 +0300

    .

---
+++ leetcode/0227.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+227. Basic Calculator II
+Difficulty: Medium
+
+"""
+from typing import List
+import time
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+
+
+sol = Solution()
+print(sol.calculate(s="3+2*2"))
+
+# stime = time.time()
+# print(f'runtime: {time.time() - stime:.2f}sec')
