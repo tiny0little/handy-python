275feda14c7a401ea0a19212319e00804c91bc53
user <nromensky@gmail.com>
2021-07-28 17:29:07 +0300

    .

--- leetcode/0301.py
+++ leetcode/0301.py
@@ -21,42 +21,51 @@ class Solution:
 
         return [left_p_count, right_p_count]
 
+    def bit_mask_increaser(self, bit_mask: List[int]) -> List[int]:
+        bit_mask[-1] += 1
+        for i in range(len(bit_mask) - 1, 0, -1):
+            if bit_mask[i] > 1:
+                bit_mask[i] = 0
+                bit_mask[i - 1] += 1
+        if bit_mask[0] > 1: bit_mask[0] = 0
+        return bit_mask
+
     def removeInvalidParentheses(self, s: str) -> List[str]:
         s = ''.join(s.split())
         s_len = len(s)
         result = []
         invalid_parentheses = self.count_invalid_parentheses(s)
+        if sum(invalid_parentheses) == 0: return [s]
+        bit_mask = [0 for _ in range(s_len)]
 
-        for i in range(s_len):
-            bit_mask = invalid_parentheses[:]
-            s_candidate = s
-            j = i
-            while j < len(s_candidate):
-                if (bit_mask[0] > 0) and (s_candidate[j] == '('):
-                    s_candidate = s_candidate[:j] + s_candidate[j + 1:]
-                    bit_mask[0] -= 1
-                    continue
-                if (bit_mask[1] > 0) and (s_candidate[j] == ')'):
-                    s_candidate = s_candidate[:j] + s_candidate[j + 1:]
-                    bit_mask[1] -= 1
-                    continue
-                if sum(bit_mask) == 0: break
-                j += 1
-            if sum(bit_mask) > 0: continue
+        counter = 2 ** s_len
+        while counter > 0:
+            # will use bit_mask which has same number of 1s as number of invalid parentheses
+            while sum(self.bit_mask_increaser(bit_mask)) != sum(invalid_parentheses): pass
+
+            # will keep bit positions which are '0'
+            s_candidate = ''
+            for i in range(len(bit_mask)):
+                if bit_mask[i] == 0:
+                    s_candidate += s[i]
+
             if self.count_invalid_parentheses(s_candidate) == [0, 0]:
                 if s_candidate not in result: result.append(s_candidate)
 
+            counter -= 1
+
+        if len(result) == 0: result.append('')
         return result
 
 
 sol = Solution()
-print(sol.removeInvalidParentheses(s=")()("))
-
-exit()
+print(sol.removeInvalidParentheses(s="()())()"))
 
 # stime = time.time()
 # print(f'runtime: {time.time() - stime:.2f}sec')
-if sol.removeInvalidParentheses(s="()())()") != ['(())()', '()()()']: print('err-1')
-if sol.removeInvalidParentheses(s="(a)())()") != ['(a())()', '(a)()()']: print('err-2')
+if sorted(sol.removeInvalidParentheses(s="()())()")) != sorted(['(())()', '()()()']): print('err-1')
+if sorted(sol.removeInvalidParentheses(s="(a)())()")) != sorted(['(a())()', '(a)()()']): print('err-2')
 if sol.removeInvalidParentheses(s=")(") != ['']: print('err-3')
 if sol.removeInvalidParentheses(s=")()(") != ['()']: print('err-53')
+if sol.removeInvalidParentheses(s="n") != ['n']: print('err-40')
+if sol.removeInvalidParentheses(s=")(f") != ['f']: print('err-43')
