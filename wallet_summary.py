548ebf1d31306708c77ae4b9fe0f97efcdaa2063
Nickolay Romensky <nromensky@gmail.com>
2021-08-01 13:37:59 +0300

    .

--- wallet_summary.py
+++ wallet_summary.py
@@ -3,44 +3,62 @@
 import subprocess
 from halo import Halo
 import time
+from typing import List
 
 blockchain_src = '/home/user/src'
 timeout_cmd = 'timeout 5'
 blockchain_list = []
 
+
+def wallet_service(blockchain: str, cmd: str):
+    blockchain_name = blockchain.split('-')[0].strip()
+    blockchain_path = f'{blockchain_src}/{blockchain}'
+
+    str0 = f"cd {blockchain_path} && . ./activate && {timeout_cmd} {blockchain_name} {cmd} wallet-only"
+    with Halo(text=f'{blockchain_name} wallet - {cmd}', color='white'): subprocess.getoutput(str0)
+
+
+def farm_state(blockchain: str, info: str, cmd: str) -> str:
+    blockchain_name = blockchain.split('-')[0].strip()
+    blockchain_path = f'{blockchain_src}/{blockchain}'
+
+    str0 = f"cd {blockchain_path} && . ./activate && {timeout_cmd} {blockchain_name} " \
+           f" farm summary 2> /dev/null | grep '{cmd}'"
+    with Halo(text=info, color='white'): result = subprocess.getoutput(str0).split(':')[1].strip()
+    return result
+
+
 str0 = f"cd {blockchain_src} && ls | grep blockchain | sort"
-if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
-    tmp_list = subprocess.getoutput(f"{str0}").split('\n')
-    for tmp0 in tmp_list:
-        blockchain_list.append([tmp0.split('-')[0], tmp0])
-else:
-    print('ERROR: no blockchain directories found')
-    exit()
+if int(subprocess.getoutput(f"{str0} | wc -l")) > 0: blockchain_list = subprocess.getoutput(f"{str0}").split('\n')
 
 for blockchain0 in blockchain_list:
-    path = f'{blockchain_src}/{blockchain0[1]}'
+    blockchain_path = f'{blockchain_src}/{blockchain0}'
+    blockchain_name = blockchain0.split('-')[0].strip()
 
-    str0 = f"cd {path} && . ./activate && {timeout_cmd} {blockchain0[0]} start wallet-only"
-    with Halo(text=f'starting {blockchain0[0]} wallet', color='white'):
-        subprocess.getoutput(str0)
+    wallet_service(blockchain0, 'start')
 
     fingerprints = []
-    str0 = f"cd {path} && . ./activate && {timeout_cmd} {blockchain0[0]} keys show | grep Fingerprint"
-    with Halo(text=f'getting fingerprints for {blockchain0[0]}', color='white'):
+    str0 = f"cd {blockchain_path} && . ./activate && {timeout_cmd} {blockchain_name} keys show | grep Fingerprint"
+    with Halo(text=f'getting fingerprints for {blockchain_name}', color='white'):
         tmp0 = subprocess.getoutput(str0).split('\n')
         for tmp1 in tmp0: fingerprints.append(tmp1.split(':')[1].strip())
 
     for fingerprint in fingerprints:
 
-        str0 = f"cd {path} && . ./activate && {timeout_cmd} {blockchain0[0]} wallet show -f {fingerprint}"
-        with Halo(text=f'waiting for {blockchain0[0]}/{fingerprint} wallet to sync', color='white'):
+        str0 = f"cd {blockchain_path} && . ./activate && {timeout_cmd} {blockchain_name} wallet show -f {fingerprint}"
+        with Halo(text=f'waiting for {blockchain_name}/{fingerprint} wallet to sync', color='white'):
             while int(subprocess.getoutput(f'{str0} | grep "Sync status: Synced" | wc -l')) != 1: time.sleep(30)
 
-        str0 = f"cd {path} && . ./activate && {timeout_cmd} {blockchain0[0]} wallet show -f {fingerprint}"
-        with Halo(text=f'getting balance of {blockchain0[0]}/{fingerprint} wallet', color='white'):
+        # str0 = f"cd {blockchain_path} && . ./activate && {timeout_cmd} {blockchain_name} wallet show -f {fingerprint}"
+        with Halo(text=f'getting balance of {blockchain_name}/{fingerprint} wallet', color='white'):
             str1 = subprocess.getoutput(f'{str0} | grep Spendable | head -1').split(':')[1].split('(')[0].strip()
-        print(f'{blockchain0[0]}/{fingerprint}: {str1}')
+        print(f'{blockchain_name}/{fingerprint}: {str1}')
 
-    str0 = f"cd {path} && . ./activate && {timeout_cmd} {blockchain0[0]} stop wallet-only"
-    with Halo(text=f'stopping {blockchain0[0]} wallet', color='white'):
-        subprocess.getoutput(str0)
+    tmp0 = farm_state(blockchain0, f'getting expected time to win for {blockchain_name}', 'Expected time to win')
+    print(f'{blockchain_name} expected time to win: {tmp0}')
+
+    tmp0 = farm_state(blockchain0, f'getting expected time to win for {blockchain_name}', 'Estimated network space')
+    print(f'{blockchain_name} network space: {tmp0}')
+
+    wallet_service(blockchain0, 'stop')
+    print()
