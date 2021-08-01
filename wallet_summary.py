9c2d3a0a89436a486c1ace3909dbcf1bf812f2fb
Nickolay Romensky <nromensky@gmail.com>
2021-08-01 11:06:59 +0300

    .

--- wallet_summary.py
+++ wallet_summary.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-import argparse
 import subprocess
 from halo import Halo
 import time
 
 blockchain_src = '/home/user/src'
 blockchain_list = []
-
-parser = argparse.ArgumentParser(description="CHIA forks wallets ")
-parser.add_argument("-s", "--show", help="show all wallets", action="store_true")
-args = parser.parse_args()
 
 str0 = f"cd {blockchain_src} && ls | grep blockchain | sort"
 if int(subprocess.getoutput(f"{str0} | wc -l")) > 0:
@@ -21,44 +16,31 @@ else:
     print('ERROR: no blockchain directories found')
     exit()
 
+for blockchain0 in blockchain_list:
+    if blockchain0[0] == 'chaingreen': continue
+    path = f'{blockchain_src}/{blockchain0[1]}'
 
-if args.show:
-    for blockchain0 in blockchain_list:
-        if blockchain0[0] == 'chaingreen': continue
+    str0 = f"cd {path} && . ./activate && {blockchain0[0]} start wallet-only"
+    with Halo(text=f'starting {blockchain0[0]} wallet', color='white'):
+        subprocess.getoutput(str0)
 
-        str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} start wallet-only"
-        with Halo(text=f'starting {blockchain0[0]} wallet', color='white'):
-            subprocess.getoutput(str0)
+    fingerprints = []
+    str0 = f"cd {path} && . ./activate && {blockchain0[0]} keys show | grep Fingerprint"
+    with Halo(text=f'getting fingerprints for {blockchain0[0]}', color='white'):
+        tmp0 = subprocess.getoutput(str0).split('\n')
+        for tmp1 in tmp0: fingerprints.append(tmp1.split(':')[1].strip())
 
-        if blockchain0[0] == 'chia':
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show -f 911097432"
-            with Halo(text=f'waiting for {blockchain0[0]} 911097432 wallet to sync', color='white'):
-                while int(subprocess.getoutput(f'{str0} | grep "Sync status: Synced" | wc -l')) != 1: time.sleep(60)
+    for fingerprint in fingerprints:
 
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show f 911097432"
-            with Halo(text=f'getting balance of {blockchain0[0]} wallet', color='white'):
-                str1 = subprocess.getoutput(f'{str0} | grep Spendable | head -1').split(':')[1].split('(')[0].strip()
-            print(f'{blockchain0[0]}: {str1}')
+        str0 = f"cd {path} && . ./activate && {blockchain0[0]} wallet show -f {fingerprint}"
+        with Halo(text=f'waiting for {blockchain0[0]}/{fingerprint} wallet to sync', color='white'):
+            while int(subprocess.getoutput(f'{str0} | grep "Sync status: Synced" | wc -l')) != 1: time.sleep(30)
 
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show -f 2876418895"
-            with Halo(text=f'waiting for {blockchain0[0]} 2876418895 wallet to sync', color='white'):
-                while int(subprocess.getoutput(f'{str0} | grep "Sync status: Synced" | wc -l')) != 1: time.sleep(60)
+        str0 = f"cd {path} && . ./activate && {blockchain0[0]} wallet show -f {fingerprint}"
+        with Halo(text=f'getting balance of {blockchain0[0]}/{fingerprint} wallet', color='white'):
+            str1 = subprocess.getoutput(f'{str0} | grep Spendable | head -1').split(':')[1].split('(')[0].strip()
+        print(f'{blockchain0[0]}/{fingerprint}: {str1}')
 
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show f 2876418895"
-            with Halo(text=f'getting balance of {blockchain0[0]} wallet', color='white'):
-                str1 = subprocess.getoutput(f'{str0} | grep Spendable | head -1').split(':')[1].split('(')[0].strip()
-            print(f'{blockchain0[0]}: {str1}')
-
-        else:
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show"
-            with Halo(text=f'waiting for {blockchain0[0]} wallet to sync', color='white'):
-                while int(subprocess.getoutput(f'{str0} | grep "Sync status: Synced" | wc -l')) != 1: time.sleep(60)
-
-            str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} wallet show"
-            with Halo(text=f'getting balance of {blockchain0[0]} wallet', color='white'):
-                str1 = subprocess.getoutput(f'{str0} | grep Spendable | head -1').split(':')[1].split('(')[0].strip()
-            print(f'{blockchain0[0]}: {str1}')
-
-        str0 = f"cd {blockchain_src}/{blockchain0[1]} && . ./activate && {blockchain0[0]} stop wallet-only"
-        with Halo(text=f'stopping {blockchain0[0]} wallet', color='white'):
-            subprocess.getoutput(str0)
+    str0 = f"cd {path} && . ./activate && {blockchain0[0]} stop wallet-only"
+    with Halo(text=f'stopping {blockchain0[0]} wallet', color='white'):
+        subprocess.getoutput(str0)
